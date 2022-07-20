8d30962248850bb60c61986b7987867868d33f7e
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-07-20 15:48:33 +0300

    Добавлены url и view для понедельника Добавлены url и view для вторника

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from week_days import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('todo_week/monday/', views.monday),
+    path('todo_week/tuesday/', views.tuesday),
 ]
