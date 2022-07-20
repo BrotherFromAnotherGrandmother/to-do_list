12c15feca956a42b4c9a1086a961d1e71f0b8ed6
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-07-20 22:20:52 +0300

    Создал динамический url для приложения week_days

--- week_days/urls.py
+++ week_days/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('monday/', views.monday),
-    path('tuesday/', views.tuesday),
+    path('<dayOfWeek>', views.get_the_day_of_the_week),
 ]
--- week_days/views.py
+++ week_days/views.py
@@ -9,3 +9,19 @@ def monday(request):
 
 def tuesday(request):
     return HttpResponse('Список дел на вторник')
+
+def get_the_day_of_the_week(request, dayOfWeek):
+    dct = {
+        'monday': 'в понедельник я жалею себя',
+        'tuesday': 'во вторник - глазею в пропасть',
+        'wednesday': 'в среду решаю проблему мирового голода (никому не говорите)',
+        'thursday': 'в четверг - зарядка',
+        'friday': 'ужин с собой, нельзя снова его пропускать',
+        'saturday': 'в субботу - борьба с презрением к себе',
+        'sunday': 'в воскресенье - иду на рождество'
+    }
+
+    if dayOfWeek in dct:
+        return HttpResponse(dct[dayOfWeek])
+    else:
+        return HttpResponse('Такого дня нет')
