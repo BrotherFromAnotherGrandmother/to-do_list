77c1808244ed859c5a3c00efed0e5111ce8593c2
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-07-26 18:17:29 +0300

    Добавил к приложению geometry все необходимые urls и views

--- geometry/views.py
+++ geometry/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def get_rectangle_area(request, width, height):
+    return HttpResponse(f'Площадь прямоугольника размером {width}x{height} равна {width * height}')
+
+
+def get_square_area(request, width):
+    return HttpResponse(f'Площадь квадрата размером {width}x{width} равна {width * width}')
+
+
+def get_circle_area(request, radius):
+    return HttpResponse(f'Площадь круга радиуса {radius} равна {3.14 * radius ** 2}')
