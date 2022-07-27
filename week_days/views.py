69a5ec711e7dbe61948b7b2620b36d1251160354
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-07-27 08:48:02 +0300

    Удалил ненужные функции в views.py приложения week_days

--- week_days/views.py
+++ week_days/views.py
@@ -3,14 +3,6 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def monday(request):
-    return HttpResponse('Список дел на понедельник')
-
-
-def tuesday(request):
-    return HttpResponse('Список дел на вторник')
-
-
 def get_the_day_of_the_week(request, dayOfWeek):
     dct = {
         'monday': 'в понедельник я жалею себя',
@@ -21,14 +13,13 @@ def get_the_day_of_the_week(request, dayOfWeek):
         'saturday': 'в субботу - борьба с презрением к себе',
         'sunday': 'в воскресенье - иду на рождество'
     }
-
     if dayOfWeek in dct:
         return HttpResponse(dct[dayOfWeek])
     else:
         return HttpResponse('Такого дня нет')
 
 
-def get_the_day_of_the_week_by_number(request, dayOfWeek:int):
+def get_the_day_of_the_week_by_number(request, dayOfWeek: int):
     if 1 <= dayOfWeek <= 7:
         return HttpResponse(f'Сегодня {dayOfWeek} день недели')
     return HttpResponse(f'Неверный номер дня - {dayOfWeek}')
